743588c53fa0649305ad1d03c3765efa0ce90f97
adam <adam@Err>
2012-12-22 09:54:31 -0600

    Updated pre-existing code in base class GuiChat for 1.4.6 changes

--- net/minecraft/src/GuiChat.java
+++ net/minecraft/src/GuiChat.java
@@ -197,7 +197,7 @@ public class GuiChat extends GuiScreen {
 			   if(var5 != null) {
 				   if(this.mc.gameSettings.chatLinksPrompt) {
 					   this.clickedURI = var5;
-					   this.mc.displayGuiScreen(new GuiChatConfirmLink(this, this, var4.getClickedUrl(), 0, var4));
+					   this.mc.displayGuiScreen(new GuiConfirmOpenLink(this, var4.getClickedUrl(), 0));
 				   } else {
 					   this.func_73896_a(var5);
 				   }
