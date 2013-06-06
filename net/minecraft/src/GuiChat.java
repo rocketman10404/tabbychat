195658eda3ec597740443a1ed503f7c85b6c285f
adam <adam@Err>
2013-06-06 15:56:15 -0500

    Committing ML files and Eclipse metadata for merged ML/LL workspace

---
+++ net/minecraft/src/GuiChat.java
@@ -0,0 +1,92 @@
+package net.minecraft.src;
+
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.GL11;
+
+import acs.tabbychat.core.ChatChannel;
+import acs.tabbychat.core.GuiChatTC;
+import acs.tabbychat.core.TabbyChat;
+import acs.tabbychat.gui.ChatButton;
+import acs.tabbychat.gui.ChatScrollBar;
+import acs.tabbychat.util.TabbyChatUtils;
+
+public class GuiChat extends GuiScreen {
+
+	protected GuiTextField inputField;
+	protected GuiChatTC wrapper;
+	public String defaultInputFieldText = "";
+	
+	public GuiChat() { }
+
+	public GuiChat(String par1Str) {
+		this.defaultInputFieldText = par1Str;
+	}
+
+	public void initGui() {
+		this.wrapper = new GuiChatTC();
+		this.wrapper.defaultInputFieldText = this.defaultInputFieldText;
+		this.wrapper.initGui();
+		this.inputField = this.wrapper.inputField;
+	}
+
+	public void onGuiClosed() {
+		Keyboard.enableRepeatEvents(false);
+		this.mc.ingameGUI.getChatGUI().resetScroll();
+	}
+
+	public void updateScreen() {
+		this.wrapper.updateScreen();
+	}
+
+	protected void keyTyped(char par1, int par2) {
+		this.wrapper.keyTyped(par1, par2);
+	}
+
+	public void handleMouseInput() {
+		if(this.wrapper != null) this.wrapper.handleMouseInput();
+		super.handleMouseInput();
+	}
+
+	protected void mouseClicked(int par1, int par2, int par3) {
+		this.wrapper.mouseClicked(par1, par2, par3);
+	}
+
+	public void confirmClicked(boolean par1, int par2) {
+		this.wrapper.confirmClicked(par1, par2);
+	}
+
+	private void func_73896_a(URI par1URI) {
+		this.wrapper.func_73896_a(par1URI);
+	}
+
+	public void completePlayerName() {
+		this.wrapper.completePlayerName();
+	}
+
+	private void func_73893_a(String par1Str, String par2Str) {
+		this.wrapper.func_73893_a(par1Str, par2Str);
+	}
+
+	public void getSentHistory(int par1) {
+		this.wrapper.getSentHistory(par1);
+	}
+
+	public void drawScreen(int par1, int par2, float par3) {
+		this.wrapper.drawScreen(par1, par2, par3);
+	}
+
+	public void func_73894_a(String[] par1ArrayOfStr) {
+		this.wrapper.func_73894_a(par1ArrayOfStr);
+	}
+
+	public boolean doesGuiPauseGame() {
+		return false;
+	}
+}
---
+++ net/minecraft/src/GuiNewChat.java
@@ -0,0 +1,109 @@
+package net.minecraft.src;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import net.minecraft.client.Minecraft;
+import org.lwjgl.opengl.GL11;
+
+import acs.tabbychat.core.ChatChannel;
+import acs.tabbychat.core.GuiNewChatTC;
+import acs.tabbychat.core.TabbyChat;
+import acs.tabbychat.settings.TimeStampEnum;
+
+public class GuiNewChat extends Gui {
+	private final Minecraft mc;
+    private final List sentMessages = new ArrayList();
+    private final List chatLines = new ArrayList();
+    private final List field_96134_d = new ArrayList();
+
+	public GuiNewChat(Minecraft par1Minecraft) {
+		this.mc = par1Minecraft;
+	}
+
+	public void drawChat(int par1) {
+		GuiNewChatTC.me.drawChat(par1);
+	}
+
+	public void clearChatMessages() {
+		GuiNewChatTC.me.clearChatMessages();
+	}
+
+	public void printChatMessage(String par1Str) {
+		GuiNewChatTC.me.printChatMessage(par1Str);
+	}
+
+	public void printChatMessageWithOptionalDeletion(String par1Str, int par2) {
+		GuiNewChatTC.me.printChatMessageWithOptionalDeletion(par1Str, par2);
+	}
+
+	public void func_96129_a(String par1Str, int par2, int par3, boolean par4) {
+		GuiNewChatTC.me.func_96129_a(par1Str, par2, par3, par4);
+	}
+
+   public void func_96132_b() {
+	   GuiNewChatTC.me.func_96132_b();
+   }
+   
+   public List getSentMessages() {
+      return GuiNewChatTC.me.getSentMessages();
+   }
+
+   public void addToSentMessages(String par1Str) {
+      GuiNewChatTC.me.addToSentMessages(par1Str);
+   }
+
+   public void resetScroll() {
+      GuiNewChatTC.me.resetScroll();
+   }
+
+   public void scroll(int par1) {
+	   GuiNewChatTC.me.scroll(par1);
+   }
+
+   public ChatClickData func_73766_a(int par1, int par2) {
+	   return GuiNewChatTC.me.func_73766_a(par1, par2);
+   }
+
+   public void addTranslatedMessage(String par1Str, Object ... par2ArrayOfObj) {
+      GuiNewChatTC.me.addTranslatedMessage(par1Str, par2ArrayOfObj);
+   }
+
+   public boolean getChatOpen() {
+      return GuiNewChatTC.me.getChatOpen();
+   }
+
+   public void deleteChatLine(int par1) {
+      GuiNewChatTC.me.deleteChatLine(par1);
+   }
+   
+   public int func_96126_f() {
+	   return func_96128_a(this.mc.gameSettings.chatWidth);
+   }
+
+   public int func_96133_g() {
+	   return func_96130_b(this.getChatOpen()?this.mc.gameSettings.chatHeightFocused:this.mc.gameSettings.chatHeightUnfocused);
+   }
+
+   public float func_96131_h() {
+	   return this.mc.gameSettings.chatScale;
+   }
+
+   public static final int func_96128_a(float par0) {
+	   short var1 = 320;
+	   byte var2 = 40;
+	   return MathHelper.floor_float(par0 * (float)(var1 - var2) + (float)var2);
+   }
+
+   public static final int func_96130_b(float par0) {
+	   short var1 = 180;
+	   byte var2 = 20;
+	   return MathHelper.floor_float(par0 * (float)(var1 - var2) + (float)var2);
+   }
+
+   public int func_96127_i() {
+	   return this.func_96133_g() / 9;
+   }
+}
